7c0c2b779c13d735c317bafb10d7db87422f971a
Semih Beyzade Örscelik <semihb.orscelik0@gmail.com>
2022-02-01 22:44:00 +0100

    window

---
+++ Window-Objesi/app.js
@@ -0,0 +1,60 @@
+// Alert
+
+// alert("Merhaba");
+
+//const cevap = confirm("Emin misiniz");
+
+//console.log(cevap);
+
+//if (cevap){
+  
+// console.log("silin gitsin");
+//}
+//else{
+  //console.log("Silmeyin");
+//}
+
+// 2.yol
+
+/* if(confirm("Emin misiniz?")){
+    console.log("silin");
+}
+else{
+    console.log("Silmeyin");
+} */
+
+/*const cevap = prompt("2 + 2 = ?");
+
+if(cevap == "4"){
+    console.log("Dogru");
+}
+else{
+    console.log("Yanlis");
+}*/
+
+let value;
+
+value = window;
+value = window.location;
+value = window.location.host;
+value = window.location.hostname;
+value = window.location.port;
+value = window.location.href;
+
+/*if(confirm("sayfa yenilensin mi?")){
+    window.location.reload();
+}
+else{
+    console.log("sayfa yenilenemedi");
+}*/
+
+value = window.outerHeight;
+value = window.outerWidth;
+
+value = window.innerHeight;
+value = window.innerWidth;
+
+value = window.scrollX;
+value = window,scrollY;
+
+console.log(value);
