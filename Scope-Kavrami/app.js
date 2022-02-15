568e26c54ee50603006919f786fbc083154d6e9d
Semih Beyzade Örscelik <semihb.orscelik0@gmail.com>
2022-02-15 21:24:53 +0100

    event

--- Scope-Kavrami/app.js
+++ Scope-Kavrami/app.js
@@ -4,28 +4,28 @@ var value1 = 10;
 let value2 = 20;
 const value3 = 30;
 
-/*function Func(){
+function Func(){
     var value1 = 40;
     let value2 = 50;
     const value3 = 60;
 
     console.log(value1,value2,value3);
 }
-Func();*/
+Func();
 
 if (true){
-    //var a = 10;
-    //let b = 20;
-    //const c = 30;
+    var a = 10;
+    let b = 20;
+    const c = 30;
     var value1 = 40;
     let value2 = 50;
     const value3 = 60;
     console.log(value1,value2,value3);
 }
 
-/*console.log(a);
-console.log(b);
-console.log(c);*/
+//console.log(a);
+//console.log(b);
+//console.log(c);
 
 
 
