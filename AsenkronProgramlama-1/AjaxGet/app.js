b7f7db01cfb7a54e775499544749d0d984e25799
Semih Beyzade Örscelik <semihb.orscelik0@gmail.com>
2022-03-23 00:30:39 +0100

    delete

--- AsenkronProgramlama-1/AjaxGet/app.js
+++ AsenkronProgramlama-1/AjaxGet/app.js
@@ -40,6 +40,41 @@ class Request {
          this.xhr.send(JSON.stringify(data));
 
     }
+
+    put(url,data,callback){
+        this.xhr.open("PUT",url);
+        this.xhr.setRequestHeader("Content-type", "application/json"); // JSON Verisi
+
+        this.xhr.onload = () => {
+            if(this.xhr.status === 200){
+                callback(null,this.xhr.responseText);
+            }
+            else{
+                callback("Hata olustu", null);
+            }
+        }
+
+        this.xhr.send(JSON.stringify(data));
+
+   }
+
+   delete(url,callback){
+    this.xhr.open("DELETE", url); // Baglanti actik
+    
+    //const temp = this;
+
+    this.xhr.onload = function(){
+        if(this.xhr.status === 200){
+            callback(null,this.xhr.responseText)
+        }
+        else{
+            callback("Hata olustu", null)
+        }
+    }.bind(this) // request'teki this i gösterir. arrow function da güzel bir metod
+
+
+    this.xhr.send();
+}
 }
 
  const request = new Request();
@@ -52,11 +87,28 @@ class Request {
  }) */
 
 
-request.post("https://jsonplaceholder.typicode.com/albums",{userId:2,title:"Thriller"},function(err,album){
+/* request.post("https://jsonplaceholder.typicode.com/albums",{userId:2,title:"Thriller"},function(err,album){
     if(err === null){
         console.log(album);
     }
     else{
         console.log(err);
     }
-})
+}) */
+
+/* request.put("https://jsonplaceholder.typicode.com/albums/10",{userId:143,title:"Tarkan karma"},function(err,album){
+    if(err === null){
+        console.log(album);
+    }
+    else{
+        console.log(err);
+    }
+}) */
+
+request.delete("https://jsonplaceholder.typicode.com/albums/10", function(err,response){
+     if(err === null){
+        console.log(response);
+     }else{
+         console.log(err);
+     }
+    })
