0ceb3c735301081a691cc177c6e32a712e41b7a2
Semih Beyzade Örscelik <semihb.orscelik0@gmail.com>
2022-03-23 00:16:27 +0100

    post

--- AsenkronProgramlama-1/AjaxGet/app.js
+++ AsenkronProgramlama-1/AjaxGet/app.js
@@ -24,14 +24,39 @@ class Request {
 
         this.xhr.send();
     }
+    post(url,data,callback){
+         this.xhr.open("POST",url);
+         this.xhr.setRequestHeader("Content-type", "application/json"); // JSON Verisi
+
+         this.xhr.onload = () => {
+             if(this.xhr.status === 201){
+                 callback(null,this.xhr.responseText);
+             }
+             else{
+                 callback("Hata olustu", null);
+             }
+         }
+
+         this.xhr.send(JSON.stringify(data));
+
+    }
 }
 
-const request = new Request();
- request.get("https://jsonplaceholder.typicode.com/albums/51", function(err,response){
+ const request = new Request();
+ /*request.get("https://jsonplaceholder.typicode.com/albums/51", function(err,response){
      if(err === null){
         console.log(response);
      }else{
          console.log(err);
      }
- })
+ }) */
 
+
+request.post("https://jsonplaceholder.typicode.com/albums",{userId:2,title:"Thriller"},function(err,album){
+    if(err === null){
+        console.log(album);
+    }
+    else{
+        console.log(err);
+    }
+})
